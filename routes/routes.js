482f24a86dcf8b1392ba030a9b0adbd8243af46b
Shane Keney <shane@spikeball.com>
2020-03-04 21:57:32 -0500

    - more project setup * folder arch * moved seed files inside db folder * created routes.js for easier route management * set up basic server.js

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+
+// Require routers here
+// ....
+
+
+
+module.exports = (app) => {
+    
+    // EX: app.use("specify base route path", specify imported router);
+    // list any other routers in same fashion as above
+}
--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-//require dotenv for env variables
-require("dotenv").config();
-// Requiring our models for syncing
+var express = require('express');
+var dotenv = require('dotenv');
 var db = require("./models");
+
+//Load environment variables if present for development
+if(process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// TODO: Possibly set up handlebars
+// ======================================
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+// ======================================
+
+// Import routes and give the server access to them
+require("./routes/routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 // { force: true }
 db.sequelize.sync().then(function () {
-    console.log("db synced");
-    db.Question.findAll({
-        include: {
-            model: db.Test,
-            where: {
-                id: 1
-            }
-        },
-    }).then(results => {
-        console.log(results[0].get({plain:true}));
-    });
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    })
 });

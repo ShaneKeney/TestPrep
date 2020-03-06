da71b98da53bf80b207d4110530a4fbbf3874083
Shane Keney <shane@spikeball.com>
2020-03-06 18:41:30 -0500

    - changed sequelize connection to use JAWS * applicable for staging and prod

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 
 fs

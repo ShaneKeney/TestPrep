36546577dce7d9c1df5f0316020c1d99871cfa12
Jeremy Marotta <firefreet@gmail.com>
2020-03-07 11:32:15 -0500

    add single route for all tests

--- controllers/api-exam-routes.js
+++ controllers/api-exam-routes.js
@@ -1,2 +1,20 @@
 // routes for getting exam information, including
 // questions detail and scoring information
+
+
+
+// Require db models
+const db = require('../models');
+
+
+module.exports = (app) => {
+    app.use('/');
+    // EX: app.use("specify base route path", specify imported router);
+    // list any other routers in same fashion as above
+    app.get('/api/exams',(req,res)=>{
+        db.Test.findAll()
+            .then((tests)=>{
+                res.json(tests);
+            });
+    });
+};

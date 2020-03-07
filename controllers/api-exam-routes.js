58fe164c3d4c015ec04b41e3a04f2b25372cbdb8
Jeremy Marotta <firefreet@gmail.com>
2020-03-07 13:12:38 -0500

    update exam routes

--- controllers/api-exam-routes.js
+++ controllers/api-exam-routes.js
@@ -7,15 +7,40 @@ const db = require('../models');
 
 module.exports = (app) => {
     // get list of all exams
-    app.get('/api/exams',(req,res)=>{
+    app.get('/api/exams', (req, res) => {
         db.Test.findAll()
-            .then((tests)=>{
+            .then((tests) => {
                 res.json(tests);
             });
     });
     // get all questions of one  section of specific exam
-    app.get('/api/exams/:examID/:section',(req,res)=>{
-        db.
-    })
+    app.get('/api/exams/:examID/:section', (req, res) => {
+        db.Test.findAll({
+            where: {
+                id: req.params.examID
+            },
+            // include: [{
+            //     model: db.Question,
+            //     where: {
+            //         section: req.params.section
+            //     }
+            // }]
+            include: [db.Question]
+        }).then((questions) => {
+            res.json(questions);
+        },
+        (error)=>{
+            console.log(error);
+        });
+    });
+
+    app.get('/api/questions',(req,res)=>{
+        db.Question.findAll({
+            include: [db.Test]
+        })
+            .then(questions=>{
+                res.json(questions);
+            });
+    });
 
 };

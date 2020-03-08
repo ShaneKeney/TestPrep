38710ad440c0c249bc893f7091680dff6f7309f9
JessButler <jessbutleruconn@gmail.com>
2020-03-07 19:58:46 -0500

    small model chgs allseeds.sql & seed files

---
+++ tests.js
@@ -0,0 +1,29 @@
+module.exports = function (seqeuelize, DataTypes) {
+    var Test = seqeuelize.define('Test', {
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        exam: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        }
+    });
+
+    Test.associate = function(models) {
+        models.Test.hasMany(models.Question, {
+            onDelete: 'CASCADE'
+        });
+    };
+
+    Test.associate = function(models) {
+        models.Test.hasMany(models.SatCurve, {
+            onDelete: 'CASCADE'
+        });
+    };
+
+    return Test;
+};

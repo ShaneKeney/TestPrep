e97b4f9c0181245ab1f9b6fc2192036ebbfaa0aa
Jeremy Marotta <firefreet@gmail.com>
2020-03-08 20:36:40 -0400

    further report route work

--- controllers/html-app-routes.js
+++ controllers/html-app-routes.js
@@ -5,14 +5,14 @@
 const db = require('../models');
 
 module.exports = (app) => {
-    // get list of all exams
+    // get get report of exam results taken by a student
     app.get('/reports/:StudentId/:TestId', (req, res) => {
         var StudentId = 1;
         var TestId = 1;
         var resultDetails;
         var sectionList;
         var questionList;
-        var score;
+        var score = 0;
         // search the sectionResultsDetails based on the StudentId and TestId
         db.SectionResultsDetails.findAll({
             where: {
@@ -22,12 +22,6 @@ module.exports = (app) => {
         })
             .then(data => {
                 resultDetails = data;
-                // search Questions based on TestId for distinct sections
-                // db.Question.aggregate('section','DISTINCT',{
-                //     where: {
-                //         TestId: TestId
-                //     }
-                // })
                 db.Question.findAll({
                     attributes: [[db.sequelize.fn('DISTINCT', db.sequelize.col('section')), 'section']],
                     where: {
@@ -48,6 +42,11 @@ module.exports = (app) => {
                                 // manually match up question list with answers by resultDetails section and question_number
                                 questionList.map(question => {
                                     // get the record from the students answers if it matches the section and question number of the current question
+                                    if (question.dataValues.section === 'mathNC' || question.dataValues.section === 'mathC') {
+                                        question.dataValues.scoreSection = 'math';
+                                    } else {
+                                        question.scoreSection = question.dataValues.section;
+                                    }
                                     const studentAnswer = resultDetails.filter(answer => {
                                         if (question.dataValues.section === answer.dataValues.section && question.dataValues.question_number === answer.dataValues.question_number) {
                                             return true;
@@ -57,10 +56,20 @@ module.exports = (app) => {
                                     if (studentAnswer) {
                                         question.dataValues.studentAnswer = studentAnswer.dataValues.answer_response;
                                     }
-                                    if (question.dataValues.studentAnswer === question.dataValues.ans_actual){
+                                    // check the validity of the student's answer
+                                    if (question.dataValues.studentAnswer === question.dataValues.ans_actual) {
                                         question.dataValues.ans_actual = '+';
                                         question.wrong = false;
                                         question.omitted = false;
+                                        switch (question.scoreSection) {
+                                        case 'reading': score.reading += 1;
+                                            break;
+                                        case 'writing': score.writing += 1;
+                                            break;
+                                        case 'math': score.math += 1;
+                                            break;
+                                        default: '';
+                                        }
                                     } else if (question.dataValues.studentAnswer) {
                                         question.wrong = true;
                                     } else {

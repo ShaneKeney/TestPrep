d36be05f6718b929d8d774b8f149fee6c2cf8076
Nathan Sartain <nathanmsartain@gmail.com>
2020-03-10 19:48:49 -0400

    Updated input validation on register form.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,5 @@
 const numArr = ['', '.', '/', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const phoneArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 $(() => {
     // const numChoices = $('.number-choices');
@@ -17,43 +18,68 @@ $(() => {
     //     $(numChoices).append(colEl);
     // }
 
-    // $('#signin-form').on('submit', e => {
-    //     e.preventDefault();
+    $('#signin-form').on('submit', e => {
+        e.preventDefault();
 
-    //     const userData = {
-    //         email: $('#signin-email').val(),
-    //         password: $('#signin-password').val()
-    //     };
+        const userData = {
+            email: $('#signin-email').val(),
+            password: $('#signin-password').val()
+        };
 
-    //     // console.log(userData);
+        // console.log(userData);
 
-    //     // $.post('/api/signin', userData)
-    //     // .then();
+        // $.post('/api/signin', userData)
+        // .then();
 
-    //     // console.log('Sign in form submitted');
-    // });
+        // console.log('Sign in form submitted');
+    });
 
     $('#register-form').on('submit', e => {
         e.preventDefault();
 
-        // make sure new password matches confirm password
-        if ($('#register-password').val() === $('#confirm-password').val()) {
+        $('#password-mismatch').addClass('d-none');
+        $('#invalid-email').addClass('d-none');
+        $('#invalid-phone').addClass('d-none');
+
+        const registerPassword = $('#register-password').val().trim();
+        const confirmPassword = $('#confirm-password').val().trim();
+        const registerEmail = $('#register-email').val().trim();
+
+        let registerPhone = '';
+
+        const validateEmail = () => {
+            const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return emailRegex.test(registerEmail);
+        }
+
+        const validatePhone = () => {
+            registerPhone = '';
+            const phoneInput = $('#register-phone').val().trim().split('');
+            phoneInput.forEach(char => {
+                if (phoneArr.includes(char)) {
+                    registerPhone.concat(char);
+                }
+            });
+
+            registerPhone.length !== 9 ? true : false;
+            
+        }
+
+        if (registerPassword === confirmPassword && validateEmail() && validatePhone()) {
             const userData = {
-                email: $('#register-email').val(),
-                phone: $('#register-phone').val(),
-                password: $('#register-password').val()
-            };
-    
+                email: registerEmail,
+                phone: registerPhone,
+                password: registerPassword
+            }
             // $.post('/api/register', userData)
             // .then();
-    
-            // console.log(userData);
-    
-            // console.log('Register form submitted');
-
-            $('#password-mismatch').addClass('d-none');
         } else {
-            $('#password-mismatch').removeClass('d-none');
+            if (registerPassword !== confirmPassword) 
+                $('#password-mismatch').removeClass('d-none');
+            if (!validateEmail())
+                $('#invalid-email').removeClass('d-none');
+            if (!validatePhone())
+                $('#invalid-phone').removeClass('d-none');
         }
     });
 

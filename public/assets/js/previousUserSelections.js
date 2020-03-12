703b6fb1687a570e9040bcaf8e9f4ffcc1dfca32
Jeremy Marotta <firefreet@gmail.com>
2020-03-11 22:40:10 -0400

    add modal to check with user which math section to review questions

--- public/assets/js/previousUserSelections.js
+++ public/assets/js/previousUserSelections.js
@@ -32,27 +32,40 @@ $('#formControlSelect1').on('input', function (event) {
     });
 });
 
-$('#formControlSelect2').on('input',function(event) {
+$('#formControlSelect2').on('input', function (event) {
     event.preventDefault();
     var testId = $('#formControlSelect1 > option:selected').attr("data-testid");
     var section = $(this).children('option:selected').text();
     location.replace(`/reports/${userId}/${testId}/${section}`);
 });
 
-$('#allSections, #thisSection').on('click',function(event){
-    // need to make the user and test parameters dynamic after it works
+$('#allSections, #thisSection').on('click', function (event) {
+    event.preventDefault();
     var endAddr = location.href.split('reports/');
     var userId = endAddr[1].split('/')[0];
     var testId = endAddr[1].split('/')[1];
     var section = $(this).attr('data-section');
     if (!section) section = 'all';
-    event.preventDefault();
-    $.ajax({
-        method: 'GET',
-        url: `/api/prevSections/${userId}/${testId}`
-    }).then(response=>{
-         localStorage.setItem('prevAnswers',JSON.stringify(response));
-        console.log(response);
-        location.replace(`/api/exams/${testId}/questions/${section}`);
-    });
+    if (section === 'math') {
+        $('#mathModal').modal();
+        $('.mathChoice').on('click', function (event) {
+            event.preventDefault();
+            section = $(this).attr('data-section');
+            getPrevSections();
+        })
+    } else {
+        getPrevSections();
+    }
+
+    function getPrevSections() {
+        $.ajax({
+            method: 'GET',
+            url: `/api/prevSections/${userId}/${testId}`
+        }).then(response => {
+            localStorage.setItem('prevAnswers', JSON.stringify(response));
+            console.log(response);
+            location.replace(`/api/exams/${testId}/questions/${section}`);
+        });
+    }
+
 });

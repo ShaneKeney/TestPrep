4327a7d96db0e9d290e55245489cc191ab103928
Jeremy Marotta <firefreet@gmail.com>
2020-03-13 12:04:12 -0400

    fix show/hide login/logout links which i just broke

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,7 +4,7 @@ const phoneArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 $(() => {
     let userCookie = getCookie('user');
-    if (userCookie.user) {
+    if (userCookie) {
         $('#register-button').addClass('d-none');
         $('#signin-button').addClass('d-none');
         $('.logout-button').removeClass('d-none');

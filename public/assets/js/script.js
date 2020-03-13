42268a58323ecb0cb2fc9a63c72f979f4524199a
Shane Keney <shane@spikeball.com>
2020-03-12 23:10:41 -0400

    - remove unnecssary commented code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,21 +2,6 @@ const numArr = ['', '.', '/', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const phoneArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 $(() => {
-    // const numChoices = $('.number-choices');
-
-    // for (let i = 0; i < 4; i++) {
-    //     const colEl = $('<div class="col-auto">');
-    //     const selEl = $('<select class="form-control">');
-        
-    //     numArr.forEach(item => {
-    //         const optionEl = $('<option>');
-    //         $(optionEl).text(item);
-    //         $(selEl).append(optionEl);
-    //     });
-
-    //     $(colEl).append(selEl);
-    //     $(numChoices).append(colEl);
-    // }
     let userCookie = getCookie('user');
     if(userCookie) {
         $('#register-button').addClass('d-none');

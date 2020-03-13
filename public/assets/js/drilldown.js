9cd7e9f9bd8f98e4c88d01e3c8a0ba9af966aef2
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-03-13 09:14:14 -0400

    cleanup

--- public/assets/js/drilldown.js
+++ public/assets/js/drilldown.js
@@ -9,27 +9,11 @@ console.log(queryString);
 
 function createQueryString(test, section, diff, cat, group){
     let query = '/?';
-    
-    if (test !== undefined){
-        query += test + '&';
-    }
-
-    if (section !== undefined){
-        query += section + '&'
-    }
-
-    if (diff !== undefined){
-        query += diff + '&'
-    }
-
-    if (cat !== undefined){
-        query += cat + '&'
-    }
-
-    if (group !== undefined){
-        query += group + '&'
-    }
-
+    if ( test !== undefined )       { query += test + '&'; }
+    if ( section !== undefined )    { query += section + '&'; }
+    if ( diff !== undefined )       { query += diff + '&'; }
+    if ( cat !== undefined )        { query += cat + '&'; }
+    if ( group !== undefined )      { query += group + '&'; }
     queryFix = query.slice(0, query.length -1);
     return queryFix;
 }

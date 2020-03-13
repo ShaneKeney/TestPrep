637742e5c021e6e07638844e1cc675ec576733b7
Jeremy Marotta <firefreet@gmail.com>
2020-03-13 10:47:48 -0400

    update to only call apis on base page load if user is logged in

--- public/assets/js/bubblesheet.js
+++ public/assets/js/bubblesheet.js
@@ -28,28 +28,29 @@ $(() => {
     } catch (err) {
         //console.log('cookie no exist!')
     }
+    if (user) {
+        //populate exam list
+        $.ajax({
+            method: 'GET',
+            url: '/api/exams',
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        }).then(results => {
+            results.forEach(test => {
+                let $option = $('<option>')
+                    .attr({
+                        'id': test.type + test.id,
+                        'data-test-id': test.id,
+                        'data-test-type': test.type,
+                    })
+                    .text(test.exam);
 
-    //populate exam list
-    $.ajax({
-        method: 'GET',
-        url: '/api/exams',
-        headers: {
-            'Authorization': `Bearer ${authToken}`
-        }
-    }).then(results => {
-        results.forEach(test => {
-            let $option = $('<option>')
-                .attr({
-                    'id': test.type + test.id,
-                    'data-test-id': test.id,
-                    'data-test-type': test.type,
-                })
-                .text(test.exam);
+                $examSelect.append($option);
+            });
 
-            $examSelect.append($option);
         });
-
-    });
+    }
 
     //populate section list
     $examSelect.on('change', function (event) {
